0f2b773fe2820d0a40e6eae93dd287ab373dd40f
milesandersonmn <82321062+milesandersonmn@users.noreply.github.com>
2022-03-06 15:00:45 +0100

    Update BestGeneFilteringProtein.R
    Edited script to simply select highest percent identity BLAST result, instead of using a hard filter

--- BestGeneFilteringProtein.R
+++ BestGeneFilteringProtein.R
@@ -5,6 +5,8 @@ sampleNames <- c("B73", "B106", "B107", "F888", "FC1890", "Lo1056", "Lo1270", "L
 
 header <- c("subject_id","query_sequence_id" ,"subject_length", "s_start", "s_end", "evalue", "bit_score", "score", "alignment_length", "identity", "query_coverage")
 
+#mRNA1 protein analysis
+
 dat.all <- list()
 best.genes.protein.Zm00001d049443_mRNA1 <- list()
 for ( j in sampleNames){
@@ -15,10 +17,6 @@ for ( j in sampleNames){
     dat <- read.table(file, sep = "\t")
     colnames(dat) <- header
     
-    #file <- paste( j, ".Zm00001d049443_mRNA1_CDS_protein.id_gene", i, ".blastp.reverse.txt", sep = "")
-    #dat.rev <- read.table(file, sep = "\t")
-    #colnames(dat.rev) <- paste(header, "_rev", sep = "")
-    
     dat.all[[i]] <- dat
     
   }
@@ -26,12 +24,14 @@ for ( j in sampleNames){
   dat.best <- lapply(dat.all, function(x){ x[order(x$identity, decreasing = T)[1],] }) %>%
     do.call( what = rbind)
   
-  dat.select <- subset(dat.best, identity > 99.351)
-  row.names(dat.select) <- j
+  highest_identity <- order(dat.best$identity, decreasing = T)
+  
+  dat.select <- dat.best[highest_identity, ]
+  row.names(dat.select[1,]) <- j
   
   
   
-  best.genes.protein.Zm00001d049443_mRNA1[[j]] <-dat.select
+  best.genes.protein.Zm00001d049443_mRNA1[[j]] <-dat.select[1, ]
 }
 
 
@@ -51,10 +51,6 @@ for ( j in sampleNames){
     dat <- read.table(file, sep = "\t")
     colnames(dat) <- header
     
-    #file <- paste( j, ".Zm00001d049443_mRNA2_CDS_protein.id_gene", i, ".blastp.reverse.txt", sep = "")
-    #dat.rev <- read.table(file, sep = "\t")
-    #colnames(dat.rev) <- paste(header, "_rev", sep = "")
-    
     dat.all[[i]] <- dat
     
   }
@@ -62,10 +58,13 @@ for ( j in sampleNames){
   dat.best <- lapply(dat.all, function(x){ x[order(x$alignment_length, decreasing = T)[1],] }) %>%
     do.call( what = rbind)
   
-  dat.select <- subset(dat.best, identity > 99)
-  row.names(dat.select) <- j
+  highest_identity <- order(dat.best$identity, decreasing = T)
+  
+  dat.select <- dat.best[highest_identity, ]
+  row.names(dat.select[1,]) <- j
   
   
   
-  best.genes.protein.Zm00001d049443_mRNA2[[j]] <-dat.select
+  best.genes.protein.Zm00001d049443_mRNA2[[j]] <-dat.select[1, ]
 }
+
