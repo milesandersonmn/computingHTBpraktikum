2fd9411f1f83db61264f8f2a2b78ec8073fc0134
milesandersonmn <82321062+milesandersonmn@users.noreply.github.com>
2022-03-07 14:34:11 +0100

    Update BestGeneFilteringmRNA.R
    Improved script to include multiple combined results beyond a hard filter of 85% query coverage in transcript x model BLAST results and model x transcript BLAST results.

--- BestGeneFilteringmRNA.R
+++ BestGeneFilteringmRNA.R
@@ -1,71 +1,130 @@
 setwd("~/BLASTResults/")
 
 library(dplyr)
-sampleNames <- c("B73", "B107", "F888", "FC1890", "Lo1056", "Lo1270", "Lo1290", "PHG83")
+sampleNames <- c("B73", "B106", "B107", "F888", "FC1890", "Lo1056", "Lo1270", "Lo1290", "PHG83")
 
-header <- c("subject_id","query_sequence_id" ,"subject_length", "s_start", "s_end", "evalue", "bit_score", "score", "alignment_length", "identity", "query_coverage")
+header <- c("subject_id","query_sequence_id" ,"subject_length", "s_start", "s_end", 
+            "evalue", "bit_score", "score", "alignment_length", "identity", 
+            "query_coverage")
 
-dat.all <- list()
+dat.fwd.all <- list()
+dat.rev.all <- list()
 
-#mRNA1 transcript analysis
+######################## mRNA1 transcript analysis ###########################
 
 best.genes.Zm00001d049443_mRNA1 <- list()
 for ( j in sampleNames){
-f <- grep(dir(), pattern = paste0( j, ".Zm00001d049443_mRNA1.id_gene"), value = T)
+
+gene_model <- "Zm00001d049443_mRNA1"  
+ 
+  
+  f <- grep(dir(), pattern = paste0( j, ".Zm00001d049443_mRNA1.id_gene"), value = T)
 
 for ( i in 1:(length(f)/2)){
   file <- paste( j, ".Zm00001d049443_mRNA1.id_gene", i, ".fa.blastn.txt", sep = "")
-  dat <- read.table(file, sep = "\t")
-  colnames(dat) <- header
+  dat.fwd <- read.table(file, sep = "\t")
+  colnames(dat.fwd) <- header
+  #rownames(dat) <- paste0( j, ".Zm00001d049443_mRNA1")
   
   file <- paste( j, ".Zm00001d049443_mRNA1.id_gene", i, ".fa.blastn.reverse.txt", sep = "")
   dat.rev <- read.table(file, sep = "\t")
   colnames(dat.rev) <- paste(header, "_rev", sep = "")
   
-  dat.all[[i]] <- cbind(dat, dat.rev)
-  
+  dat.fwd.all[[i]] <- dat.fwd
+  dat.rev.all[[i]] <- dat.rev
 }
 
-dat.best <- lapply(dat.all, function(x){ x[order(x$alignment_length, decreasing = T)[1],] }) %>%
+for ( s in 1:length(dat.fwd.all)){
+  if ( nrow(dat.fwd.all[[s]]) > 1 ){
+    longest_alignment <- order(dat.fwd.all[[s]]$alignment_length, decreasing = T)[1]
+    dat.fwd.all[[s]] <- dat.fwd.all[[s]][longest_alignment, ]
+  } 
+}  
+for ( s in 1:length(dat.rev.all)){
+  if ( nrow(dat.rev.all[[s]]) > 1 ){
+    longest_alignment <- order(dat.rev.all[[s]]$alignment_length_rev, decreasing = T)[1]
+    dat.rev.all[[s]] <- dat.rev.all[[s]][longest_alignment, ]
+  } 
+}
+  
+dat.fwd.best <- lapply(dat.fwd.all, function(x){ x[order(x$subject_id, decreasing = T)[1],] }) %>%
   do.call( what = rbind)
 
-dat.select <- subset(dat.best, query_coverage > 90 & query_coverage_rev > 90)
-row.names(dat.select) <- j
+dat.fwd.select <- subset(dat.fwd.best, query_coverage > 85)
 
+dat.rev.best <- lapply(dat.rev.all, function(x){ x[order(x$alignment_length, decreasing = T)[1],] }) %>%
+  do.call( what = rbind)
 
+dat.rev.select <- subset(dat.rev.best, query_coverage_rev > 85)
+
+dat.rev.select <- dat.rev.select[match(dat.fwd.select$subject_id, dat.rev.select$query_sequence_id_rev),]
+
+dat.select <- cbind(dat.fwd.select, dat.rev.select)
+dat.select['sample']=j
+dat.select['gene_model']=gene_model
 
      best.genes.Zm00001d049443_mRNA1[[j]] <-dat.select
 }
 
-#mRNA2 transcript analysis
+best.genes <- bind_rows(best.genes.Zm00001d049443_mRNA1)
 
-sampleNames <- c("B73", "B107", "F888", "FC1890", "Lo1056", "Lo1270", "Lo1290", "PHG83")
 
+############### mRNA2 transcript analysis ####################
 
 best.genes.Zm00001d049443_mRNA2 <- list()
 for ( j in sampleNames){
+  
+  gene_model <- "Zm00001d049443_mRNA2"  
+  
+  
   f <- grep(dir(), pattern = paste0( j, ".Zm00001d049443_mRNA2.id_gene"), value = T)
   
   for ( i in 1:(length(f)/2)){
     file <- paste( j, ".Zm00001d049443_mRNA2.id_gene", i, ".fa.blastn.txt", sep = "")
-    dat <- read.table(file, sep = "\t")
-    colnames(dat) <- header
+    dat.fwd <- read.table(file, sep = "\t")
+    colnames(dat.fwd) <- header
+    #rownames(dat) <- paste0( j, ".Zm00001d049443_mRNA2")
     
     file <- paste( j, ".Zm00001d049443_mRNA2.id_gene", i, ".fa.blastn.reverse.txt", sep = "")
     dat.rev <- read.table(file, sep = "\t")
     colnames(dat.rev) <- paste(header, "_rev", sep = "")
     
-    dat.all[[i]] <- cbind(dat, dat.rev)
-    
+    dat.fwd.all[[i]] <- dat.fwd
+    dat.rev.all[[i]] <- dat.rev
   }
   
-  dat.best <- lapply(dat.all, function(x){ x[order(x$alignment_length, decreasing = T)[1],] }) %>%
+  for ( s in 1:length(dat.fwd.all)){
+    if ( nrow(dat.fwd.all[[s]]) > 1 ){
+      longest_alignment <- order(dat.fwd.all[[s]]$alignment_length, decreasing = T)[1]
+      dat.fwd.all[[s]] <- dat.fwd.all[[s]][longest_alignment, ]
+    } 
+  }  
+  for ( s in 1:length(dat.rev.all)){
+    if ( nrow(dat.rev.all[[s]]) > 1 ){
+      longest_alignment <- order(dat.rev.all[[s]]$alignment_length_rev, decreasing = T)[1]
+      dat.rev.all[[s]] <- dat.rev.all[[s]][longest_alignment, ]
+    } 
+  }
+  
+  dat.fwd.best <- lapply(dat.fwd.all, function(x){ x[order(x$subject_id, decreasing = T)[1],] }) %>%
     do.call( what = rbind)
   
-  dat.select <- subset(dat.best, query_coverage > 95 & query_coverage_rev > 90)
-  row.names(dat.select) <- j
+  dat.fwd.select <- subset(dat.fwd.best, query_coverage > 85)
   
+  dat.rev.best <- lapply(dat.rev.all, function(x){ x[order(x$alignment_length, decreasing = T)[1],] }) %>%
+    do.call( what = rbind)
   
+  dat.rev.select <- subset(dat.rev.best, query_coverage_rev > 85)
+  
+  dat.rev.select <- dat.rev.select[match(dat.fwd.select$subject_id, dat.rev.select$query_sequence_id_rev),]
+  
+  dat.select <- cbind(dat.fwd.select, dat.rev.select)
+  dat.select['sample']=j
+  dat.select['gene_model']=gene_model
   
   best.genes.Zm00001d049443_mRNA2[[j]] <-dat.select
+  
 }
+
+x <- bind_rows(best.genes.Zm00001d049443_mRNA2)
+best.genes <- rbind(best.genes, x)
